23d5d5371a569f773f74006f18b378200b014663
Akshay <akshayvashisht15@gmail.com>
2024-03-13 22:34:25 -0400

    Main method. Basic testing

--- create_card.py
+++ create_card.py
@@ -33,11 +33,11 @@ def read_json(file):
   card_list = []
 
   #Iterate through JSON file and get all fields
-  for i in range(len(f_loaded['card-info'])):
-    kanji = f_loaded["card-info-items"][i]['card-info'][0]['front']['kanji']
-    english_translation = f_loaded["card-info-items"][i][1]['back']['english_translation']
-    example_sentence = f_loaded["card-info-items"][i][1]['back']['example_sentence']
-    english_example_sentence = f_loaded["card-info-items"][i][1]['back']['english_example_sentence']
+  for i in range(len(f_loaded['card-info-items'])):
+    kanji = f_loaded["card-info-items"][i]['card-info']['front']['kanji']
+    english_translation = f_loaded["card-info-items"][i]['card-info']['back']['english_translation']
+    example_sentence = f_loaded["card-info-items"][i]['card-info']['back']['example_sentence']
+    english_example_sentence = f_loaded["card-info-items"][i]['card-info']['back']['english_example_sentence']
 
     #Create card_info_tuple used to store fields
     card_info_tuple = (kanji, english_translation, example_sentence, english_example_sentence)
@@ -53,7 +53,7 @@ Iterates through a list of cards, creates a card and adds it to an existing deck
 
 Parameters: card_list (List), deck (genanki.Deck)
 '''
-def create_card(card_list, deck=genanki.Deck):
+def create_card(card_list, deck: genanki.Deck):
   #Iterate through the card list and add card to deck
   for card in card_list:
     note = genanki.Note(model= my_model, fields=[card[0], card[1], card[2], card[3]] )
@@ -61,5 +61,14 @@ def create_card(card_list, deck=genanki.Deck):
     #Add card to deck
     deck.add_note(note)
 
+'''
+Main method
+'''
+def main():
+  card_list1 = read_json("test-1.json")
+  deck1 = genanki.Deck(1, "Deck1")
+  create_card(card_list1, deck1)
+  genanki.Package(deck1).write_to_file("Deck1")
 
-
+if __name__ == "__main__":
+    main()
